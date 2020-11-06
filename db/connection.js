606645813e1d00e02e83f5b37fd7040542253336
Jon <churchjg283@gmail.com>
2020-11-06 11:33:37 -0500

    deploying

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,25 @@
 const mongoose = require("mongoose");
 
+let mongoURI = "";
+if(process.env.NODE_ENV === "production"){
+    mongoURI = process.env.DB_URL;
+}else{
+    mongoURI = "mongodb://localhost/foodinfo"
+}
+
+mongoose.connect(mongoURI, { useNewUrlParser: true },{ useUnifiedTopology: true } );
+
+/*
+module.exports = mongoose;
 mongoose
-  .connect("mongodb://localhost/foodinfo", { 
+  .connect("mongodb://localhost/foodinfo", {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  /*.then(instance =>
-    console.log(`Connected to db`)
-  )
-  .catch(err => console.log("Connection Failed.", err));
+/*.then(instance =>
+  console.log(`Connected to db`)
+)
+.catch(err => console.log("Connection Failed.", err));
 */
 // mongoose.connect("mongodb://localhost/foodinfo", { useNewUrlParser: true });
 
